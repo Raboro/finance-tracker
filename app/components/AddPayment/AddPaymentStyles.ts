272276b84d86eef3f4f5d8ff7b1a296b13f9d238
Raboro <raborogit@gmail.com>
2023-09-13 22:23:14 +0200

    added AddPaymentModal with closing button

---
+++ app/components/AddPayment/AddPaymentStyles.ts
@@ -0,0 +1,22 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+   
+    container: {
+        flex: 1,
+        padding: 60, 
+        borderRadius: 20, 
+        marginVertical: 80,
+        marginHorizontal: 20,
+        backgroundColor: '#add8e6',
+        borderWidth: 5,
+        borderColor: '#6495ed'
+    },
+
+    closeIcon: {
+        position: 'absolute',
+        right: 0,
+        marginRight: 10,
+        marginTop: 10
+    }
+});
