623d5d1dcd275a052b4e7b3d7c02809ebef88e4a
Raboro <raborogit@gmail.com>
2023-09-14 22:43:22 +0200

    added Payment component

---
+++ app/components/Payment/PaymentItem.tsx
@@ -0,0 +1,8 @@
+import Payment from "../../logic/Payment";
+import { Text } from 'react-native';
+import { styles } from "./PaymentStyles";
+
+
+export default function PaymentItem({ payment }: { payment: Payment }) {
+    return <Text style={styles.container}>{payment.value}</Text>;
+}
---
+++ app/components/Payment/PaymentStyles.ts
@@ -0,0 +1,11 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+    container: {
+        borderWidth: 3,
+        margin: '3%',
+        borderRadius: 10,
+        textAlign: 'center',
+        marginHorizontal: '20%'
+    }
+});
