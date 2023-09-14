dc0df42438a406da4bbe132e9e941ee9a263baf6
Raboro <raborogit@gmail.com>
2023-09-14 22:45:00 +0200

    added PaymentHistory component

---
+++ app/components/PaymentHistory/PaymentHistory.tsx
@@ -0,0 +1,17 @@
+import { FlatList, Text, View } from "react-native";
+import Payment from "../../logic/Payment";
+import { styles } from './PaymentStyles';
+import PaymentItem from "../Payment/PaymentItem";
+
+
+export default function PaymentHistory({ payments }: { payments: Payment[] }) {
+    return (
+        <View style={styles.container}>
+            <Text style={styles.heading}>List of Payments</Text>
+            <FlatList
+                data={payments}
+                renderItem={({ item }) => <PaymentItem payment={item} />}
+            />   
+        </View>
+    );
+}
---
+++ app/components/PaymentHistory/PaymentStyles.ts
@@ -0,0 +1,19 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+    container: {
+        width: '75%',
+        borderWidth: 3,
+        flex: 1,
+        justifyContent: 'center',
+        marginBottom: '30%',
+        marginTop: '15%',
+        borderRadius: 15
+    },
+
+    heading: {
+        textAlign: 'center', 
+        marginVertical: '5%',
+        fontWeight: 'bold'
+    }
+});
