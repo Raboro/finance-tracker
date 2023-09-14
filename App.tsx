923e4af2452a4a745c26884999db3c131444f7b4
Raboro <raborogit@gmail.com>
2023-09-14 22:46:16 +0200

    replaced default text with PaymentHistory

--- App.tsx
+++ App.tsx
@@ -1,16 +1,21 @@
 import React, { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, StyleSheet } from 'react-native';
 import Footer from './app/components/Footer/Footer';
 import Balance from './app/components/Balance/Balance';
 import Settings from './app/components/Settings/Settings';
 import AddPayment from './app/components/AddPayment/AddPayment';
+import Payment from './app/logic/Payment';
+import PaymentHistory from './app/components/PaymentHistory/PaymentHistory';
 
 export default function App() {
   const [balance, setBalance] = useState(0);
   const [addPaymentVisibility, setAddPaymentVisibility] = useState(false);
+  const [payments, updatePayments] = useState<Payment[]>([]);;
 
   const updateBalance = (update: number) => {
+    const value: number = update.toString() === 'NaN' ? 0 : update;
+    updatePayments(() => [...payments, new Payment(value)]);
     setBalance(balance + (update.toString() === 'NaN' ? 0 : update));
   };
 
@@ -20,11 +25,7 @@ export default function App() {
       <Settings />
       <Balance balance={balance} />
 
-      <View style={styles.content}>
-        <Text style={styles.text}>
-          Open up App.tsx to start working on your app!
-        </Text>
-      </View>
+      <PaymentHistory payments={payments}/>
 
       <AddPayment
         visibility={addPaymentVisibility}
@@ -43,14 +44,5 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
-  },
-
-  content: {
-    flex: 1,
-    justifyContent: 'center',
-  },
-
-  text: {
-    textAlign: 'center',
   },
 });
