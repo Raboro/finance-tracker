02780c94ff54537a4f9cb5550f3bcdb6a039da4d
Raboro <raborogit@gmail.com>
2023-09-16 15:57:53 +0200

    extracted default border

---
+++ app/utils/Border.ts
@@ -0,0 +1,11 @@
+import { StyleSheet } from "react-native";
+import { COLORS } from "./ColorStyles";
+
+export const border = StyleSheet.create({
+    
+    default: {
+        borderColor: COLORS.primary,
+        borderRadius: 15,
+        borderWidth: 3
+    }
+});
