feb7567d96ba8ea75f533bcde30df694cf5e1459
Raboro <raborogit@gmail.com>
2023-09-17 13:39:43 +0200

    added necessary methods and fields to Payment

--- app/logic/Payment.ts
+++ app/logic/Payment.ts
@@ -1,7 +1,17 @@
 export default class Payment {
+  private id: string;
   value: number;
 
-  constructor(value: number) {
+  constructor(value: number, id?: string) {
+    this.id = id ?? '';
     this.value = value;
+  }
+
+  getKey(): string {
+    return this.id;
+  }
+
+  toStorage(): string {
+    return JSON.stringify(this);
   }
 }
