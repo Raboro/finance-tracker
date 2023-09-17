03505e03946f16e637a1fdb0aff03c3875868431
Raboro <raborogit@gmail.com>
2023-09-17 20:14:30 +0200

    added animation & presentation style

--- app/components/PaymentItemOptionsModal/PaymentItemOptionsModal.tsx
+++ app/components/PaymentItemOptionsModal/PaymentItemOptionsModal.tsx
@@ -1,14 +1,16 @@
-import ClosingIcon from "../ClosingIcon/ClosingIcon";
-import { Modal } from "react-native";
+import ClosingIcon from '../ClosingIcon/ClosingIcon';
+import { Modal } from 'react-native';
 
 interface PaymentItemOptionsModalProps {
-    visibilityChange: (change: boolean) => void;
+  visibilityChange: (change: boolean) => void;
 }
 
-export default function PaymentItemOptionsModal(props: PaymentItemOptionsModalProps) {
-    return (
-        <Modal>
-            < ClosingIcon visibilityChange={props.visibilityChange}/>
-        </Modal>
-    );
+export default function PaymentItemOptionsModal(
+  props: PaymentItemOptionsModalProps,
+) {
+  return (
+    <Modal animationType="slide" presentationStyle="pageSheet">
+      <ClosingIcon visibilityChange={props.visibilityChange} />
+    </Modal>
+  );
 }
