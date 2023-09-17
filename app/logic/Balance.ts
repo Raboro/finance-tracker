311fd40e710cb2652c690c48da2647a3a86e38e2
Raboro <raborogit@gmail.com>
2023-09-17 17:19:24 +0200

    integrate storage in to Balance

--- app/logic/Balance.ts
+++ app/logic/Balance.ts
@@ -1,14 +1,27 @@
 import Payment from './Payment';
+import Storage from './Storage';
 
 export default class Balance {
+  private storage: Storage = new Storage();
   private payments: Payment[] = [];
   private value: number = 0;
 
-  addPayments(...payments: Payment[]): void {
-    payments.forEach((p) => this.payments.push(p));
+
+  async fetchPayments(): Promise<void> {
+    this.payments = await this.storage.getAllPayments()
   }
 
-  updatePayment(payment: Payment): void {
+  addPayments(...payments: number[]): void {
+    payments.forEach(async (payment) => {
+      const key = await this.storage.generateKey(); 
+      const paymentObj = new Payment(payment, key);
+      await this.storage.insertPayment(paymentObj);
+      this.payments.push(paymentObj);
+    });
+  }
+
+  async updatePayment(payment: Payment): Promise<void> {
+    await this.storage.updatePayment(payment);
     for (let i = 0; i < this.payments.length; i++) {
       if (this.payments[i].getKey() === payment.getKey()) {
         this.payments[i] = payment;
@@ -17,13 +30,20 @@ export default class Balance {
     }
   }
 
-  removePayment(id: string): void {
+  async removePayment(id: string): Promise<void> {
+    await this.storage.removePayment(id);
     this.payments = this.payments.filter((p) => p.getKey() !== id);
   }
 
-  recalculate(): number {
+  async recalculate(): Promise<number> {
+    await this.fetchPayments();
     this.value = 0;
     this.payments.forEach((p) => (this.value += p.value));
     return this.value;
+  }
+
+  async getPayments(): Promise<Payment[]> {
+    await this.fetchPayments();
+    return this.payments;
   }
 }
