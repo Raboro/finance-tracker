f98810a3386aa0530e5c5da50ebd2273c3f73280
Raboro <raborogit@gmail.com>
2023-09-17 20:52:09 +0200

    extracted PressableButton component

---
+++ app/components/PressableButton/PressableButton.tsx
@@ -0,0 +1,25 @@
+import { Text, Pressable, StyleSheet, StyleProp, ViewStyle } from "react-native";
+import { border } from "../../utils/Border";
+import { styles } from "./PressableButtonStyling";
+
+interface PressableButtonProps {
+    text: string,
+    onPress: () => void;
+    style?: StyleProp<ViewStyle>;
+}
+
+export default function PressableButton(props: PressableButtonProps) {
+    return <>
+        <Pressable
+            style={({ pressed }) => [
+              styles.button,
+              border.default,
+              pressed && styles.pressedButton,
+              props.style ?? {}
+            ]}
+            onPress={props.onPress}
+          >
+            <Text style={styles.text}>{props.text}</Text>
+          </Pressable>
+    </>
+}
