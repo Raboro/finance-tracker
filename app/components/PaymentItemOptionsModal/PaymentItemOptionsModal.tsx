30855b542a9d179c090b1011e7b2b4e6fca030bf
Raboro <raborogit@gmail.com>
2023-09-17 20:40:08 +0200

    added edit and delete buttons

--- app/components/PaymentItemOptionsModal/PaymentItemOptionsModal.tsx
+++ app/components/PaymentItemOptionsModal/PaymentItemOptionsModal.tsx
@@ -1,6 +1,7 @@
 import ClosingIcon from '../ClosingIcon/ClosingIcon';
-import { Modal, View } from 'react-native';
+import { Text, Modal, View, Pressable } from 'react-native';
 import { styles } from './PaymentItemOptionsModalStyles';
+import { border } from '../../utils/Border';
 
 interface PaymentItemOptionsModalProps {
   visibilityChange: (change: boolean) => void;
@@ -13,6 +14,26 @@ export default function PaymentItemOptionsModal(
     <Modal animationType="slide" presentationStyle="pageSheet">
       <View style={styles.container}>
         <ClosingIcon visibilityChange={props.visibilityChange} />
+        <View style={styles.buttonContainer}>
+          <Pressable
+            style={({ pressed }) => [
+              styles.button,
+              border.default,
+              pressed && styles.pressedButton,
+            ]}
+          >
+            <Text style={styles.text}>Edit</Text>
+          </Pressable>
+          <Pressable
+            style={({ pressed }) => [
+              styles.button,
+              border.default,
+              pressed && styles.pressedButton,
+            ]}
+          >
+            <Text style={styles.text}>Delete</Text>
+          </Pressable>
+        </View>
       </View>
     </Modal>
   );
--- app/components/PaymentItemOptionsModal/PaymentItemOptionsModalStyles.ts
+++ app/components/PaymentItemOptionsModal/PaymentItemOptionsModalStyles.ts
@@ -12,4 +12,33 @@ export const styles = StyleSheet.create({
     borderWidth: 5,
     borderColor: COLORS.primary,
   },
+
+  buttonContainer: {
+    justifyContent: 'center',
+    flexDirection: 'row',
+    gap: 10,
+    marginTop: '15%'
+  },
+
+  button: {
+    right: 0,
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingVertical: 12,
+    paddingHorizontal: 32,
+    elevation: 3,
+    backgroundColor: COLORS.tertiary,
+  },
+
+  pressedButton: {
+    opacity: 0.9,
+    transform: [{ scaleX: 1.05 }, { scaleY: 1.05 }],
+  },
+
+  text: {
+    lineHeight: 21,
+    fontWeight: 'bold',
+    letterSpacing: 0.25,
+    color: 'black',
+  },
 });
