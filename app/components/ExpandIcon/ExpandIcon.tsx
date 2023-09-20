69b24e77be440d82dbaac37f7211b954eac7f058
Raboro <raborogit@gmail.com>
2023-09-21 00:04:11 +0200

    used TouchableOpacity to wrap icon

--- app/components/ExpandIcon/ExpandIcon.tsx
+++ app/components/ExpandIcon/ExpandIcon.tsx
@@ -1,4 +1,5 @@
 import { MaterialIcons } from '@expo/vector-icons';
+import { TouchableOpacity } from 'react-native';
 
 interface ExpandIconProps {
   expand: boolean;
@@ -7,11 +8,14 @@ interface ExpandIconProps {
 
 export default function ExpandIcon(props: ExpandIconProps) {
   return (
-    <MaterialIcons
-      name={props.expand ? 'expand-less' : 'expand-more'}
-      size={35}
-      color="black"
-      onPress={() => props.changeState(!props.expand)}
-    />
+    <>
+      <TouchableOpacity testID="ExpandIcon" onPress={() => props.changeState(!props.expand)}>
+        <MaterialIcons
+          name={props.expand ? 'expand-less' : 'expand-more'}
+          size={35}
+          color="black"
+        />
+      </TouchableOpacity>
+    </>
   );
 }
