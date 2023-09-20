fd62449df7482d0576b03b4b3bd2afd323886504
Raboro <raborogit@gmail.com>
2023-09-20 13:27:33 +0200

    fixed jest config

--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,6 @@
 module.exports = {
-  preset: 'react-native',
-  moduleFileExtensions: ['ts', 'tsx', 'js'],
-  testRegex: '(/__tests__/.*|\\.(test|spec))\\.(ts|tsx|js)$',
-  testPathIgnorePatterns: [
-    '\\.snap$',
-    './node_modules/',
-    './lib/',
-    './coverage/',
-  ],
-  cacheDirectory: '.jest/cache',
+  preset: "jest-expo",
+  transformIgnorePatterns: [
+    "node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg)"
+  ]
 };
