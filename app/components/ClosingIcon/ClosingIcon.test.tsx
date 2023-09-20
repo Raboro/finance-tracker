7818262d366b2062e581422d8b92a6423b662fb5
Raboro <raborogit@gmail.com>
2023-09-20 23:19:27 +0200

    added test for closingIcon

---
+++ app/components/ClosingIcon/ClosingIcon.test.tsx
@@ -0,0 +1,13 @@
+import { describe, expect, jest, test } from '@jest/globals';
+import { fireEvent, render } from '@testing-library/react-native';
+import ClosingIcon from './ClosingIcon';
+
+describe('ClosingIcon', () => {
+  test('buttonClick', () => {
+    const mockOnPress = jest.fn();
+    const rend = render(<ClosingIcon visibilityChange={mockOnPress}/>)
+    const button = rend.getByTestId('ClosingIcon');
+    fireEvent(button, 'press');
+    expect(mockOnPress).toHaveBeenCalled();
+  });
+});
