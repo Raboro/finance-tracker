e5bf24405e8f60be7f4988909d243543e8c0514b
Raboro <raborogit@gmail.com>
2023-09-20 13:27:47 +0200

    added tests for addPayment

---
+++ app/components/AddPayment/AddPayment.test.tsx
@@ -0,0 +1,52 @@
+import renderer from 'react-test-renderer';
+import { describe, expect, jest, test } from '@jest/globals';
+import AddPayment from './AddPayment';
+import { fireEvent, render } from '@testing-library/react-native';
+
+describe('AddPayment', () => {
+    test('renderNotNull', () => {
+        const r = renderer.create(
+            <AddPayment 
+                visibility={true}
+                setAddPaymentVisibility={() => {}}
+                updateBalance={() => {}}
+            />
+        );
+        expect(r).not.toBe(null);  
+    });
+
+    test('update input with submit', () => {
+        const updateBalance = jest.fn();
+        const rend = render(
+            <AddPayment 
+                visibility={true}
+                setAddPaymentVisibility={() => {}}
+                updateBalance={updateBalance}
+            />
+        );
+        const inputElement = rend.getByPlaceholderText('Payment');
+        fireEvent.changeText(inputElement, '50');
+        const submitButton = rend.getByText('Submit');
+        fireEvent.press(submitButton);
+        expect(updateBalance).toHaveBeenCalledWith(50); 
+    });
+
+    test('invalid input should be NuN', () => {
+        const updateBalance = jest.fn();
+        const rend = render(
+            <AddPayment 
+                visibility={true}
+                setAddPaymentVisibility={() => {}}
+                updateBalance={updateBalance}
+            />
+        );
+        const inputElement = rend.getByPlaceholderText('Payment');
+        fireEvent.changeText(inputElement, '');
+        const submitButton = rend.getByText('Submit');
+        fireEvent.press(submitButton);
+        expect(updateBalance).toHaveBeenCalledWith(NaN); 
+        fireEvent.changeText(inputElement, 'Text');
+        fireEvent.press(submitButton);
+        expect(updateBalance).toHaveBeenCalledWith(NaN);  
+    });
+});
