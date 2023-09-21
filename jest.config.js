9aa2a49af2d3d5819af42c73a46f6aafca7342c5
Raboro <marius.woerfelreal@gmail.com>
2023-09-21 14:08:57 +0200

    excluded files and folder from test and test:coverage

--- jest.config.js
+++ jest.config.js
@@ -3,4 +3,10 @@ module.exports = {
   transformIgnorePatterns: [
     'node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg)',
   ],
+  coveragePathIgnorePatterns: [
+    "/node_modules/",
+    "/babel.config.js",
+    "/jest.config.js",
+    "/coverage/"
+  ],
 };
